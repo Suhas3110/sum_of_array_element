4438bb9bfb5ef68d08a6fc00fe665fd3170a3df2
suhas <suhas@DELL>
2024-12-03 15:48:27 +0530

    Add project files.

---
+++ sum_of_array_element/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_of_array_element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]arr=new int[5];
+            int sum = 0;
+            Console.WriteLine("Enter the array Element");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i]=int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Sum of array Element");
+            for(int i = 0;i < arr.Length; i++)
+            {
+                sum=sum+arr[i];
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sum_of_array_element/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("sum_of_array_element")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sum_of_array_element")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8ee3090e-72ed-4880-bcc1-2bae01397e61")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
